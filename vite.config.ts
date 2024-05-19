f5a6518b7e0bcaae76616b310fd150d175c4cc00
0xhttps <mjquinn11@gmail.com>
2024-05-19 16:44:48 -0700

    fix #2

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,11 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'process.env': process.env
-  },
   build: {
-    outDir: 'dist', // Output directory for the build
+    outDir: 'dist',
   },
   base: '/0xhttps-web3/', // Set base path for GitHub Pages
 });
