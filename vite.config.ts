29c0447c1eb987627780d60224ba423481d1619f
0xhttps <mjquinn11@gmail.com>
2024-05-23 19:18:30 -0700

    fix gh-pages route after repo rename

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 const isGithubActions = process.env.GITHUB_ACTIONS || false;
-const base = isGithubActions ? '/0xhttps-web3/' : '/';
+const base = isGithubActions ? '/personal/' : '/';
 
 export default defineConfig({
   plugins: [react()],
