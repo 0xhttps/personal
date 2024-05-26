373e33da6860d006359b65c435baa474094a6ea5
0xhttps <mjquinn11@gmail.com>
2024-05-26 00:09:34 -0700

    terminal styling + home page

--- src/components/util/TerminalComponent.tsx
+++ src/components/util/TerminalComponent.tsx
@@ -3,15 +3,29 @@ import { useTerminalContext } from './TerminalContext';
 
 const TerminalComponent: React.FC = () => {
   const terminalRef = useRef<HTMLDivElement>(null);
-  const { initTerminal } = useTerminalContext();
+  const { initTerminal, term } = useTerminalContext();
 
   useEffect(() => {
     if (terminalRef.current) {
       initTerminal(terminalRef.current);
     }
-  }, [terminalRef, initTerminal]);
+  }, [initTerminal, term]);
 
-  return <div ref={terminalRef} style={{ width: '100%', height: '100%', backgroundColor: '#1E1E1E' }} />;
+  return (
+    <div 
+      ref={terminalRef} 
+      style={{ 
+        width: '100%', 
+        height: '100%', 
+        backgroundColor: '#1E1E1E',
+        whiteSpace: 'pre-wrap',
+        overflow: 'hidden',
+        wordWrap: 'break-word',
+        paddingTop: 10,
+        paddingLeft: 10,
+      }} 
+    />
+  );
 };
 
 export default TerminalComponent;
--- src/components/util/TerminalOverlay.tsx
+++ src/components/util/TerminalOverlay.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
-import { Modal, Box, Backdrop, Fade } from '@mui/material';
+import React, { useEffect } from 'react';
+import { Modal, Box, Backdrop, Fade, IconButton } from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
 import TerminalComponent from './TerminalComponent';
 import { useTerminalContext } from './TerminalContext';
 
 const TerminalOverlay: React.FC = () => {
-  const { open, handleClose } = useTerminalContext();
+  const { open, handleClose, term } = useTerminalContext();
+
+  useEffect(() => {
+    if (open && term) {
+      term.focus();
+    }
+  }, [open, term]);
 
   return (
     <Modal
@@ -16,6 +23,7 @@ const TerminalOverlay: React.FC = () => {
     >
       <Fade in={open}>
         <Box
+          tabIndex={-1}
           sx={{
             position: 'absolute',
             top: '50%',
@@ -25,10 +33,36 @@ const TerminalOverlay: React.FC = () => {
             height: '80%',
             bgcolor: 'background.paper',
             boxShadow: 24,
-            paddingTop: 4,
+            display: 'flex',
+            flexDirection: 'column',
+            borderRadius: '8px', // Optional: Add rounded corners
           }}
         >
-          <TerminalComponent />
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'flex-end',
+              alignItems: 'center',
+              height: '40px', // Set header height
+              padding: '0 10px',
+              bgcolor: 'background.default',
+              borderBottom: '1px solid',
+              borderColor: 'divider',
+            }}
+          >
+            <IconButton
+              aria-label="close"
+              onClick={handleClose}
+              sx={{
+                color: (theme) => theme.palette.grey[500],
+              }}
+            >
+              <CloseIcon />
+            </IconButton>
+          </Box>
+          <Box sx={{ flex: 1, overflow: 'hidden', display: 'flex', flexDirection: 'column' }}>
+            <TerminalComponent />
+          </Box>
         </Box>
       </Fade>
     </Modal>
