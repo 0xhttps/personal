4cb0e0c2c33265da474a2d04bc2f540033bce2fc
0xhttps <mjquinn11@gmail.com>
2024-07-07 11:52:31 -0700

    .

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-const base = isGithubActions ? '/personal/' : '/';
-
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
   },
-  base,
 });
