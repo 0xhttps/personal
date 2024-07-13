87d13f9261d54b8de5c70ef545819a336b5ce727
0xhttps <mjquinn11@gmail.com>
2024-07-13 13:27:43 -0700

    add vite compression

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import compression from 'vite-plugin-compression';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    compression({
+      verbose: true,
+      disable: false,
+      threshold: 10240,
+      algorithm: 'gzip',
+      ext: '.gz',
+    }),
+  ],
   server: {
     port: 4441
   },
