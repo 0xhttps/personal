bb403e8695f9f2f13cae00dc6410457e00a054ec
0xhttps <mjquinn11@gmail.com>
2024-07-13 19:06:21 -0700

    remove compression

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import compression from 'vite-plugin-compression';
 
 export default defineConfig({
   plugins: [
     react(),
-    compression({
-      verbose: true,
-      disable: false,
-      threshold: 10240,
-      algorithm: 'gzip',
-      ext: '.gz',
-    }),
   ],
   server: {
     port: 4441,
